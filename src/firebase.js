67227b75833f5bccc1e2136697107770dcd91ff6
YujunSun0 <cons202@gmail.com>
2023-10-20 14:11:30 +0900

    FireBase SDK 추가

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBiQPiC4BL8DE9NVq3Oc5EYtZjUtsO86wo",
+  authDomain: "open-market-31c8b.firebaseapp.com",
+  projectId: "open-market-31c8b",
+  storageBucket: "open-market-31c8b.appspot.com",
+  messagingSenderId: "816762749704",
+  appId: "1:816762749704:web:acb032283d52ecfa8962bd",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
