6ab68cd69b31c66eb3811ff6520fd66ab0cc7070
YujunSun0 <cons202@gmail.com>
2023-10-24 15:02:06 +0900

    메인 색상 반영 및 회원가입 버튼 추가

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,7 +1,10 @@
 import { Link } from 'react-router-dom'
 import styles from "./login.module.css"
+import { useNavigate } from 'react-router-dom'
 
 export const Login = () => {
+    const navigate = useNavigate();
+
     return (
     <div className={styles.container}>
             <div className={styles.box}>
@@ -12,7 +15,10 @@ export const Login = () => {
                 <input className={styles.input_id} type="email" placeholder="id"/>
                 <input className={styles.input_id} type="password" placeholder="password"/>
                     <button className={styles.login_btn}>로그인</button>
-            </div>
+                </div>
+                <p className={styles.p}>
+                    계정이 없으신가요? <span className={styles.span} onClick={() => navigate("/signup")}>회원가입</span>
+                </p>
             </div>
     </div>
     )
