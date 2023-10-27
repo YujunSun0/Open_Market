dbe6fb752738ceb0f9bd99c165a720ff39282232
YujunSun0 <cons202@gmail.com>
2023-10-27 15:49:21 +0900

    이메일 중복확인 함수 작성

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,16 @@
 // Import the functions you need from the SDKs you need
 import firebase from "firebase/compat/app"
 import "firebase/compat/firestore"
+import { getAuth } from "firebase/auth"
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  addDoc,
+  collection,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,4 +25,22 @@ const firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 export const firestore = firebase.firestore();
+export const authService = getAuth();
+
+// 이메일 중복확인
+export const checkDuplicateEmailWithFirebase = async (email) => {
+  try {
+    // db 중복 확인
+    const q = query(collection(db, "users"), where("email", "==", email));
+    const querySnapshot = await getDocs(q);
+    return querySnapshot.empty ? false : true; // 이미 있는 계정이라면 false, 없는 계정이면 true를 
+    
+  } catch (error) {
+    console.log("check email", error);
+  }
+};
